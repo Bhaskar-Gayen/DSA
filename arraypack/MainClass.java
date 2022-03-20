d373291d0b9690574f252c0461231756f24b5efc
gayenbhaskar007@gmail.com <gayenbhaskar007@gmail.com>
2022-03-20 22:45:01 +0530

    Merge two sorted array

--- arraypack/MainClass.java
+++ arraypack/MainClass.java
@@ -4,6 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainClass {
+	/* Merge two sorted array */
+	public void merge(int[] nums1, int m, int[] nums2, int n) {
+        //edge cases 
+        if(n==0) return;
+
+        int i=m-1,j=n-1,k=m+n-1;
+       while(j>=0){
+           if(i>=0&&nums1[i]>nums2[j])
+               nums1[k--]=nums1[i--];
+           else
+               nums1[k--]=nums2[j--];
+       }
+    }
 	
 	/* Maximum array sum from both end of a array */
 	public int maxSumEnd(ArrayList<Integer> A, int B){
