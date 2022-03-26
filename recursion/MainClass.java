c69819494f78faf8ab2edf23ac3a6d056aaee6d2
gayenbhaskar007@gmail.com <gayenbhaskar007@gmail.com>
2022-03-26 12:43:49 +0530

    LeetCode-46-Permutations

--- recursion/MainClass.java
+++ recursion/MainClass.java
@@ -5,6 +5,44 @@ import java.util.Collections;
 import java.util.List;
 
 public class MainClass {
+	/*LeetCode: 46. Permutations
+	 * Input: nums = [1,2,3]
+		Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+		*/
+	public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        int index=0;
+        solver(nums,ans,index);
+        
+        return ans;
+    }
+    
+    static void solver(int[] arr, List<List<Integer>> ans, int index){
+        //base case
+        if(index>=arr.length){
+            List<Integer> t = new ArrayList<>();
+            for(int i: arr)
+                t.add(i);
+            ans.add(t);
+            return;
+        }
+        
+        //process
+        for(int i=index;i<arr.length;i++){
+            swap(arr,i,index);
+            solver(arr,ans,index+1);
+            //Backtrack
+            swap(arr,i,index);
+        }
+    }
+    
+    static void swap(int[] arr, int i, int j){
+        int t=arr[i];
+        arr[i]=arr[j];
+        arr[j]=t;
+    }
+	
+	
 	
 	/*Code Studio: Permutations of a String
 	 * input: xyz
