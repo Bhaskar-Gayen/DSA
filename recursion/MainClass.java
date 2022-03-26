305b9588eb3df765fc859fc3d38dbdebf3a7911d
gayenbhaskar007@gmail.com <gayenbhaskar007@gmail.com>
2022-03-26 12:00:17 +0530

    Code Studio-Permutations of a String

--- recursion/MainClass.java
+++ recursion/MainClass.java
@@ -1,9 +1,45 @@
 package recursion;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MainClass {
+	
+	/*Code Studio: Permutations of a String
+	 * input: xyz
+	 * output: xyz xzy yxz yzx zxy zyx */
+	public static ArrayList<String> generatePermutations(String str) {
+		StringBuilder input=new StringBuilder(str);
+        ArrayList<String> ans = new ArrayList<>();
+        int index=0;
+        solver(input,ans,index);
+        Collections.sort(ans);
+        return ans;
+	}
+    static void solver(StringBuilder str, ArrayList<String> ans, int index){
+        //base case
+        if(index>=str.length()){
+            ans.add(str.toString());
+            return;
+        }
+        //process
+        for(int i=index;i<str.length();i++){
+            swap(str,i,index);
+            solver(str,ans,index+1);
+            swap(str,i,index);
+        }
+        
+    }
+    static void swap(StringBuilder str, int index, int i){
+        char c = str.charAt(index);
+        str.setCharAt(index,str.charAt(i));
+        str.setCharAt(i,c);
+    }
+	
+	
+	
+	
 	
 	/*LeetCode: 17. Letter Combinations of a Phone Number 
 	 * Input: digits = "23"
