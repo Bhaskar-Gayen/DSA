2e0900dca86687586c6e3ad702b3525694d8b105
gayenbhaskar007@gmail.com <gayenbhaskar007@gmail.com>
2022-03-31 11:11:46 +0530

    Definition for singly-linked list

--- linkList/ReverseList.java
+++ linkList/ReverseList.java
@@ -2,21 +2,21 @@ package linkList;
 
 /*LeetCode: 206. Reverse Linked List*/
 public class ReverseList {
-	public ListNode reverseList(ListNode head) {
-        //handle base case
-        if(head==null||head.next==null)
-            return head;
-        ListNode prev, curr, nextNode;
-        prev=null;
-        curr=head;
-        while(curr!=null){
-            nextNode=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=nextNode;
-        }
-        
-        return prev; 
-    }
+	  //Iterative way to reverse a LinkList.
+		public ListNode reverseList(ListNode head) {
+	        //handle base case
+	        if(head==null||head.next==null)
+	            return head;
+	        ListNode prev, curr, nextNode;
+	        prev=null;
+	        curr=head;
+	        while(curr!=null){
+	            nextNode=curr.next;
+	            curr.next=prev;
+	            prev=curr;
+	            curr=nextNode;
+	        } 
+	        return prev;
+		}
 
 }
