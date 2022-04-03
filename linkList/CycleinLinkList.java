daacd8d552d40e2f39d70dda8ad8b168f2e44447
gayenbhaskar007@gmail.com <gayenbhaskar007@gmail.com>
2022-04-03 13:24:16 +0530

    remove cycle fro Link List

--- linkList/CycleinLinkList.java
+++ linkList/CycleinLinkList.java
@@ -10,7 +10,7 @@ import java.util.Set;
  * Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
 */
 public class CycleinLinkList {
-	/*Method 1*/	
+	/*Method 1(Floyd Cycle Detection Algorithm)*/	
     public boolean hasCycle(ListNode head) {
         if(head==null)
             return false;
@@ -44,7 +44,7 @@ public class CycleinLinkList {
         return false;
     }
     
-    /* return starting node of a cycle in a link list */ 
+    /* Method 1: return starting node of a cycle in a link list */ 
     public ListNode detectCycle(ListNode head) {
         if(head==null||head.next==null)
            return null;
@@ -58,5 +58,52 @@ public class CycleinLinkList {
        }
        return null;
    }
+    
+    /*Method 2: return starting node of a cycle in a link list */
+    public ListNode detectCycle2(ListNode head) {
+        if(head==null||head.next==null)
+           return null;
+       ListNode slow=head;
+       ListNode fast= cycle(head);
+       if(fast!=null){
+            while(slow!=fast){
+           slow=slow.next;
+           fast=fast.next;
+                }
+           return slow;
+       }
+       return null;
+       
+   }
+   
+   static ListNode cycle(ListNode head){
+       if(head==null||head.next==null)
+           return null;
+       ListNode slow=head,fast=head;
+       while(fast!=null&&fast.next!=null){
+           slow=slow.next;
+           fast=fast.next.next;
+           if(slow==fast)
+               return fast;
+       }
+       return null;
+   }
+   
+   /* Remove Cycle from Link List */
+   ListNode removeLoop(ListNode head) {
+	      if(head==null || head.next==null)
+	            return null;
+	      ListNode start=detectCycle2(head);
+	      ListNode temp=start;
+	      if(temp!=null){
+	         while(temp.next!=start)
+	           temp=temp.next;
+	        temp.next=null;
+	      }     
+	      return head;
+	          
+	      
+	  }
+   
 
 }
