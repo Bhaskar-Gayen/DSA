24b2f40e483e301d415d4ad16321c2185f6a59a1
Bhaskar <gayenbhaskar007@gmail.com>
2022-08-13 00:47:50 +0530

    Refactor the code

--- dp/UniquePath.java
+++ dp/UniquePath.java
@@ -3,41 +3,66 @@ package dp;
 import java.util.Arrays;
 
 public class UniquePath {
-	
-	/*Solution using Dynamic Programming-T.C=O(r*c) S.C=O(r*C)*/
-	 public int uniquePaths(int r, int c) {
-	        int[][] dp =new int[r][c];
-	        for(int[] i:dp)
-	        Arrays.fill(i,-1);
-	        
-	        
-	        return findPathDp(r,c,0,0,dp);
-	    }
-	    
-	    
-	    //Solution using DP
-	    int findPathDp(int r, int c, int x, int y, int[][] dp){
-	        if(x>=r||y>=c)
-	            return 0;
-	            // System.out.println(x+" "+y);
-	        if((x==r-1)&&(y==c-1))
-	            return dp[x][y]=1;
-	        
-	            
-	        if(dp[x][y]!=-1)
-	            return dp[x][y];
-	        
-	         return dp[x][y]= findPathDp(r,c,x+1,y,dp)+findPathDp(r,c,x,y+1,dp);
-	    }
-	    
-	    
-	    //Time complexity is Exponential O(2^n)
-	    int findPath(int r, int c, int x, int y){
-	        if(x>r||y>c)
-	            return 0;
-	        if(x==r&&c==y)
-	            return 1;
-	        return findPath(r,c,x+1,y)+findPath(r,c,x,y+1);
-	    }
+
+	/* Solution using Dynamic Programming-T.C=O(r*c) S.C=O(r*C) */
+	public static int uniquePaths(int r, int c) {
+		int[][] dp = new int[r][c];
+		for (int[] i : dp)
+			Arrays.fill(i, -1);
+
+		return loopDpSpaceOptimization(r, c);
+	}
+
+	/* recursion */
+	static int recursionSolution(int r, int c) {
+		if (r == 0 & c == 0)
+			return 1;
+		if (r < 0 || c < 0)
+			return 0;
+		return recursionSolution(r, c - 1) + recursionSolution(r - 1, c);
+	}
+
+	/* recursion + memorization = top-down */
+	static int recursionDP(int r, int c, int[][] dp) {
+		if (r == 0 & c == 0)
+			return dp[r][c] = 1;
+		if (r < 0 || c < 0)
+			return 0;
+		if (dp[r][c] != -1)
+			return dp[r][c];
+		return dp[r][c] = recursionDP(r, c - 1, dp) + recursionDP(r - 1, c, dp);
+	}
+
+	/* loop + memorization = bottom-up */
+	static int loopDP(int r, int c, int[][] dp) {
+		for (int i = 0; i < r; i++) {
+			for (int j = 0; j < c; j++) {
+				if (i == 0 && j == 0)
+					dp[i][j] = 1;
+				else {
+					int left = j > 0 ? dp[i][j - 1] : 0;
+					int right = i > 0 ? dp[i - 1][j] : 0;
+					dp[i][j] = left + right;
+				}
+			}
+		}
+		return dp[r - 1][c - 1];
+	}
+
+	/* Space optimization */
+	static int loopDpSpaceOptimization(int r, int c) {
+		int[] dp = new int[c];
+
+		for (int i = 0; i < c; i++)
+			dp[i] = 1;
+
+		for (int i = 1; i < r; i++) {
+			for (int j = 1; j < c; j++) {
+				dp[j] += dp[j - 1];
+			}
+		}
+
+		return dp[c - 1];
+	}
 
 }
